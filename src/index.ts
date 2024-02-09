dd72b290b11ca1bf47be6b9c76d07dd82b8df1c5
Mohammed Falih <yunusfalih123@gmail.com>
2024-02-09 22:18:57 +0530

    typescript changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid';
 
-const form = document.getElementById('new-task-title') as HTMLFormElement | null;
+const form = document.getElementById('new-task-form') as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
 const list = document.querySelector<HTMLUListElement>('#list');
 
@@ -11,7 +11,7 @@ type Task = {
     createdAt: Date
 }
 
-form?.addEventListener('submit', e => {
+form?.addEventListener("submit", e => {
     e.preventDefault()
 
     if (input?.value == "" || input?.value == null) return
@@ -22,4 +22,21 @@ form?.addEventListener('submit', e => {
         completed: false,
         createdAt: new Date()
     }
+
+    addListItem(newTask);
+    input.value = ""
 })
+
+function addListItem(task: Task) {
+    
+    const item = document.createElement('li');
+    const label = document.createElement('label');
+    const checkBox = document.createElement('input');
+    checkBox.type = "checkbox"
+    checkBox.checked = task.completed;
+    
+    label.append(checkBox, task.title);
+    item.append(label);
+    list?.append(item);
+
+}
